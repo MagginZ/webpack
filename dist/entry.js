5b188482b890caeddd34498c49562c594c688e6c
ZengMeiqi <1350155894@qq.com>
2018-06-01 10:28:04 +0800

    [webpack_demo] + 分离less&sass

---
+++ dist/entry.js
@@ -0,0 +1,13 @@
+!function(e){var _={};function r(s){if(_[s])return _[s].exports;var n=_[s]={i:s,l:!1,exports:{}};return e[s].call(n.exports,n,n.exports,r),n.l=!0,n.exports}r.m=e,r.c=_,r.d=function(e,_,s){r.o(e,_)||Object.defineProperty(e,_,{enumerable:!0,get:s})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,_){if(1&_&&(e=r(e)),8&_)return e;if(4&_&&"object"==typeof e&&e&&e.__esModule)return e;var s=Object.create(null);if(r.r(s),Object.defineProperty(s,"default",{enumerable:!0,value:e}),2&_&&"string"!=typeof e)for(var n in e)r.d(s,n,function(_){return e[_]}.bind(null,n));return s},r.n=function(e){var _=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(_,"a",_),_},r.o=function(e,_){return Object.prototype.hasOwnProperty.call(e,_)},r.p="",r(r.s="./src/entry.js")}({"./src/css/black.less":
+/*!****************************!*\
+  !*** ./src/css/black.less ***!
+  \****************************/
+/*! no static exports found */function(module,exports){eval("// removed by extract-text-webpack-plugin\n\n//# sourceURL=webpack:///./src/css/black.less?")},"./src/css/index.css":
+/*!***************************!*\
+  !*** ./src/css/index.css ***!
+  \***************************/
+/*! no static exports found */function(module,exports){eval("// removed by extract-text-webpack-plugin\n\n//# sourceURL=webpack:///./src/css/index.css?")},"./src/entry.js":
+/*!**********************!*\
+  !*** ./src/entry.js ***!
+  \**********************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css_index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./css/index.css */ \"./src/css/index.css\");\n/* harmony import */ var _css_index_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_css_index_css__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _css_black_less__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./css/black.less */ \"./src/css/black.less\");\n/* harmony import */ var _css_black_less__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_css_black_less__WEBPACK_IMPORTED_MODULE_1__);\n\n\ndocument.getElementById('title').innerHTML = 'this is a base demo'\n\n\n//# sourceURL=webpack:///./src/entry.js?")}});
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path')
+// 引入ugligyjs-webpack-glugin插件，自动集成，不用安装，用于压缩js
+const uglify = require('uglifyjs-webpack-plugin')
+// 打包html文件到生产路径，引入插件 安装 $ sudo npm install --save-dev html-webpack-plugin
+const htmlPlugin = require('html-webpack-plugin')
+// css分离 引入插件  webpack 3.x 安装 sudo npm install --save-dev extract-text-webpack-plugin
+const extractTextPlugin = require("extract-text-webpack-plugin")
+
+module.exports = {
+    // 入口
+    entry: {
+        entry: './src/entry.js',
+        entry2: './src/enrty2.js'
+    }, 
+     // 出口
+    output: {
+        path: path.resolve(__dirname, 'dist'), // 出口文件绝对路径
+        // filename: 'bundle.js' // 出口文件打包
+        // 多入口打包
+        filename: '[name].js'
+    },
+    // 所有模块配置
+    module: {
+        // 配置cssloader
+        rules: [
+            {
+                test: /\.css$/, // 用于匹配处理文件的拓展名的表达式，必须
+                // use: ['style-loader', 'css-loader'], // loader名称，必须
+                // inclue/exclude ：手动添加必须处理的文件(可选)
+                // query: 为loaders提供额外的设置选项(可以选)
+                // 分离css 需要修改loader为：
+                use: extractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            // 安装配置图片打包的loader
+            // npm install --save-dev file-loader url-loader
+            {
+                test: /\.(png|jpg|gif)/,  // 匹配图片后缀
+                use: [{
+                    loader: 'url-loader', // 使用指定的loader
+                    options: {
+                        limit: 500000, // limit参数：把小于500000B的文件打包成base64的格式，写入js
+                        outputPath: 'images/' // 打包图片到images路径
+                    }
+                }]
+            },
+            // 解决html 文件中引入img标签的
+            {
+                test: /\.(htm|html)$/i,
+                use: ['html-withimg-loader']
+            },
+            // 配置less-loader
+            {
+                test: /\.less$/,
+                use: extractTextPlugin.extract({
+                    use: [
+                        // {
+                        //     loader: "style-loader" // 分离less不这样写
+                        // }, 
+                        {
+                            loader: "css-loader"
+                        }, {
+                            loader: "less-loader"
+                        }],
+                        fallback: "style-loader"
+                })
+                
+            }
+        ]
+    }, 
+    // 插件
+    plugins: [
+        new uglify(),
+        // 配置html打包
+        new htmlPlugin({
+            minify: { // 对html文件压缩
+                removeAttributeQuotes: true //去掉属性的双引号
+            },
+            hash: true, // 为了避免缓存js
+            template: './src/index.html' // 要打包的html模板路径和文件名称
+        }),
+        // 配置插件熟悉
+        new extractTextPlugin('css/index.css') // 这里的/css是分离后的路径 需要修改css-loader 和 style-loader
+    ],
+    // 开发服务 
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'), // 设置基本目录结构
+        host: 'localhost', // 服务器发访问地址
+        compress: true, // 服务器端压缩是否开启
+        port: 1717 // 端口号
+    } 
+}
